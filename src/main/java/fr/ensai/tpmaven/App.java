0fdb30166011989a96c227c4783324690f052d5f
bagayogo <bg.baggas@gmail.com>
2015-09-23 15:27:15 +0200

    new

---
+++ src/main/java/fr/ensai/tpmaven/App.java
@@ -0,0 +1,14 @@
+package fr.ensai.tpmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	//DEBUT DU PROGRAMME
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
